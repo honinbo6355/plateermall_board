68022c02974add421d5245be30f554275b64eb8b
JEONEK <jeonek@plateer.com>
2020-03-24 15:48:56 +0900

    Faq detail frame

--- shoppingmall_board-rest/src/main/java/com/plateer/controller/BoardController.java
+++ shoppingmall_board-rest/src/main/java/com/plateer/controller/BoardController.java
@@ -12,14 +12,21 @@ import java.util.List;
         allowedHeaders = {"Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
                 "Access-Control-Request-Headers", "Access-Control-Allow-Origin", "Set-Cookie"},
         exposedHeaders = {"Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"}, maxAge = 3000)
-@RequestMapping("/api/board/")
+@RequestMapping("/api/faq/")
 public class BoardController {
 
-    @GetMapping("faq-list")
-    public List<BoardDto> getBoardDto() {
+    @GetMapping("list")
+    public List<BoardDto> getFaqList() {
         List<BoardDto> list = new ArrayList<>();
         list.add(new BoardDto(1,"123","123","123","123",true,true));
         list.add(new BoardDto(2,"456","456","456","456",false,false));
         return list;
+    }
+
+    @GetMapping("detail")
+    public BoardDto getFaq() {
+
+
+        return null;
     }
 }
