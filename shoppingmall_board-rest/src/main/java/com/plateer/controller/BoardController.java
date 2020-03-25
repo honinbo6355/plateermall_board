5d8ecf210db7c8a2a67b2ec81f1ee39eee06132f
JEONEK <jeonek@plateer.com>
2020-03-25 14:13:51 +0900

    질문/답변 게시판 - Question List, Answer List, Question Detail

--- shoppingmall_board-rest/src/main/java/com/plateer/controller/BoardController.java
+++ shoppingmall_board-rest/src/main/java/com/plateer/controller/BoardController.java
@@ -5,7 +5,6 @@ import com.plateer.domain.dto.QuestionDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -16,35 +15,61 @@ import java.util.List;
 @RequestMapping("/api/faq/")
 public class BoardController {
 
+    private List<QuestionDto> questionList = new ArrayList<>();
+    private List<AnswerDto> answerList = new ArrayList<>();
+
+    public BoardController() {
+        questionList.add(new QuestionDto(1, true, null, "주문영역질문", "123", "나이키신발질문", "왜이렇게큰가요", true, true));
+        questionList.add(new QuestionDto(2, false, null, "배송영역질문", "456", "꽃무늬원피스질문", "왜이렇게작은가요", false, false));
+
+        answerList.add(new AnswerDto(1, null, "관리자", "당신의 발이 큰 탓입니다."));
+        answerList.add(new AnswerDto(2, null, "관리자", "당신이 살이 찐 탓입니다.."));
+    }
+
     @GetMapping("question/list")
     public List<QuestionDto> getQuestionList() {
-        List<QuestionDto> list = new ArrayList<>();
-        list.add(new QuestionDto(1,true,null,"주문영역질문","123","나이키신발질문","왜이렇게큰가요",true,true));
-        list.add(new QuestionDto(2,false,null,"배송영역질문","456","꽃무늬원피스질문","왜이렇게작은가요",false,false));
-        return list;
+        return questionList;
     }
 
     @GetMapping("question/detail/{postId}")
     public QuestionDto getQuestion(@PathVariable int postId) {
-        List<QuestionDto> list = new ArrayList<>();
-        list.add(new QuestionDto(1,true,null,"주문영역질문","123","나이키신발질문","왜이렇게큰가요",true,true));
-        list.add(new QuestionDto(2,false,null,"배송영역질문","456","꽃무늬원피스질문","왜이렇게작은가요",false,false));
 
-        QuestionDto foundedBoard = null;
+        QuestionDto foundedQuestion = null;
 
-        for(int i=0 ; i<list.size() ; i++) {
-            if(list.get(i).getPostId()==postId) {
-                foundedBoard = list.get(i);
+        for(int i=0; i < questionList.size(); i++) {
+            if (questionList.get(i).getPostId() == postId) {
+                foundedQuestion = questionList.get(i);
             }
         }
-        return foundedBoard;
+        return foundedQuestion;
     }
 
     @GetMapping("answer/{postId}")
-    public AnswerDto getAnswer(@PathVariable String postId) {
+    public AnswerDto getAnswer(@PathVariable int postId) {
 
+        AnswerDto foundedAnswer = null;
 
+        for(int i=0; i < answerList.size(); i++) {
+            if (answerList.get(i).getPostId() == postId) {
+                foundedAnswer = answerList.get(i);
+            }
+        }
+        return foundedAnswer;
+    }
 
-        return null;
+    @PostMapping("question/registration/")
+    public void questionRegistration(@RequestBody QuestionDto questionDto) {
+        questionList.add(new QuestionDto(
+                        3,
+                        false,
+                        questionDto.getDate(),
+                        questionDto.getTerritory(),
+                        questionDto.getGoodsCode(),
+                        questionDto.getTitle(),
+                        questionDto.getDescription(),
+                        questionDto.isSmsAlarm(),
+                        questionDto.isEmailAlarm()
+                )
+        );
     }
 }
